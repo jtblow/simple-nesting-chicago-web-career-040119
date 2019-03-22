95d7ea7084376b3510196514f267894c69f8e245
creative-zombie-4458 <jtbartlow@gmail.com>
2019-03-22 05:39:14 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-    programmer_hash[:dennis_ritchie][:languages].replace(:languages => "C")
+    programmer_hash[:dennis_ritchie].replace(:languages => "C")
      programmer_hash[:dennis_ritchie][:languages]
 end
 
